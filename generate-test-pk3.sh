6a9c65c432b07aeeec7087fcc99eab5330a4af72
silvertear <silvertear@node-3.net>
2011-12-26 21:23:01 -0700

    Added a script to generate a playtest version of legendary

---
+++ generate-test-pk3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+FILES=$(ls | grep -vE '.pk3|.sh')
+CURRENT_REVISION=$(git log --pretty=format:'%h' -n 1)
+
+if [ -f legendary-$CURRENT_REVISION.pk3 ]; then
+	rm -rf legendary-$CURRENT_REVISION.pk3
+fi
+
+zip -9r legendary-$CURRENT_REVISION.pk3 $FILES -x *.backup* *.dbs *.bak
