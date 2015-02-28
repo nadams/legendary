c7ab8133ecab2af611bbff6d9f9874dc6316e5aa
silvertear <silvertear@node-3.net>
2015-02-28 09:46:53 -0700

    Imporving diff script

--- generate-diff-pk3.sh
+++ generate-diff-pk3.sh
@@ -1,26 +1,17 @@
 #!/bin/sh
 
 if [ -z $1 ]; then
-	echo "Remote branch name required"
+	echo "Remote branch name required."
 	exit 1
+fi
+
+if [ -f legendary-diff-$CURRENT_REVISION.pk3 ]; then
+	rm -f legendary-diff-$CURRENT_REVISION.pk3
 fi
 
 SOURCE_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 REMOTE_BRANCH=$1
-
-DIFF_FILES=$(git diff --name-only $SOURCE_BRANCH..$REMOTE_BRANCH)
-DEST="diff"
-mkdir -p $DEST
-cp -Rv $DIFF_FILES $DEST
-cd $DEST
-
-if [ -f legendary-diff-$CURRENT_REVISION.pk3 ]; then
-	rm -rf legendary-diff-$CURRENT_REVISION.pk3
-fi
-
-FILES=$(ls | grep -vE '.pk3|.sh')
 CURRENT_REVISION=$(git log --pretty=format:'%h' -n 1)
+FILES=$(git diff --name-only $SOURCE_BRANCH..$REMOTE_BRANCH)
 
 zip -9r legendary-diff-$CURRENT_REVISION.pk3 $FILES -x *.backup* *.dbs *.bak *.db
-
-cd ..
